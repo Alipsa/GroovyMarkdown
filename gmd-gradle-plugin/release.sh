2a8e34c20cfadbaa86ee807d72310870ecd930a7
per <per@alipsa.se>
2026-02-07 16:51:08 +0100

    improved release process

--- gmd-gradle-plugin/release.sh
+++ gmd-gradle-plugin/release.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
-./gradlew clean build publishToMavenLocal
+pushd ..
+  version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+popd
+
+./gradlew clean build publishToMavenLocal -Ppublish.version=$version
+
 pushd src/test/manual-test
   ./gradlew clean build
 popd
-./gradlew clean build publishPlugins --validate-only
-./gradlew publishPlugins
+
+./gradlew clean build publishPlugins --validate-only -Ppublish.version=$version
+./gradlew publishPlugins -Ppublish.version=$version
--- release.sh
+++ release.sh
@@ -2,4 +2,5 @@
 
 echo "publishing all from parent + all sub projects"
 mvn -Prelease deploy
-echo "To finish publishing visit https://central.sonatype.com/publishing/deployments"
+echo "To check central publishing visit https://central.sonatype.com/publishing/deployments"
+echo "To check gradle plugin portal publishing visit https://plugins.gradle.org/plugin/se.alipsa.gmd.gmd-gradle-plugin"
